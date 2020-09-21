830c3d988db5adb9ec0d69e01bda42fffcad6450
lizeyang1995 <424011527@qq.com>
2020-09-21 21:50:52 +0800

    [lizeyang] faet: introduce with name, age and class number, given teacher have class

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,13 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+  }
+}
