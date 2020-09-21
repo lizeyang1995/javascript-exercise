f4ccc5b7928e9acb647ed31b859a8d3e5a9c554e
lizeyang1995 <424011527@qq.com>
2020-09-21 22:58:21 +0800

    [lizeyang] faet: using XMLHttpRequest

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,8 +2,15 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
-  // end -->
+  xhr.open('get', url);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+    if (xhr.status === 200) {
+      successCallback(xhr.responseText);
+    } else {
+      errorCallback(xhr.statusText);
+    }
+  };
 }
 
 const URL = 'http://localhost:3000/api';
