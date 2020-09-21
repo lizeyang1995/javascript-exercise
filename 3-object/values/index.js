ed065f5d62c4499d0959b3132a1d91d8cd008c9a
lizeyang1995 <424011527@qq.com>
2020-09-21 20:25:25 +0800

    [lizeyang] faet: count Types Number

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,7 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const sum = Object.values(source).reduce((previousValue, currentValue) => {
+    return Number(currentValue) + previousValue;
+  }, 0);
+  return sum;
 }
