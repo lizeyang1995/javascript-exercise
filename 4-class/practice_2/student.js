ca5053dcdbb38be65aebcafecdfb709dce17a825
lizeyang1995 <424011527@qq.com>
2020-09-21 20:51:06 +0800

    [lizeyang] faet: should have field name, age and class number

--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,1 +1,9 @@
+import Person from './person';
+
 // TODO 11: 在这里写实现代码
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+}
