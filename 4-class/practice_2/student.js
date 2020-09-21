76a57f0f9e690bc0b9aeeca3484f70dde3af548f
lizeyang1995 <424011527@qq.com>
2020-09-21 20:57:11 +0800

    [lizeyang] faet: overwrite Person introduce, introduce student with class

--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -6,4 +6,8 @@ export default class Student extends Person {
     super(name, age);
     this.klass = klass;
   }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
 }
