9a86870d47454a50339292a33456a9067e3cee4d
lizeyang1995 <424011527@qq.com>
2020-09-21 21:37:32 +0800

    [lizeyang] faet: introduce with name and age, but different with Person introduce

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,8 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Worker. I have a job.`;
+  }
+}
