75371677fbdaa44e458c07b4df682041a8661ba4
lizeyang1995 <424011527@qq.com>
2020-09-21 21:17:28 +0800

    [lizeyang] faet: overwrite Person introduce, introduce with no field

--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,4 +1,11 @@
 import Person from './person';
 
 // TODO 14: 在这里写实现代码
-export default class Worker extends Person {}
+export default class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+    this.introduce = () => {
+      return 'I am a Worker. I have a job.';
+    };
+  }
+}
